93fc3daa75732f4338e2e153f44b331f738a45fa
NathanRodriguez04 <naterod26@gmail.com>
2021-08-16 09:05:29 -0500

    blog

--- src/main/java/com/codeup/blogapp/web/PostsController.java
+++ src/main/java/com/codeup/blogapp/web/PostsController.java
@@ -42,4 +42,21 @@ public class PostsController {
         }
     }
 
+    @PostMapping
+    private void createPost(@RequestBody Post newPost){
+        System.out.println(newPost.getTitle());
+        System.out.println(newPost.getContent());
+    }
+    @PutMapping("/{id}")
+    private void updatePost(@PathVariable Long id, @RequestBody Post post){
+        System.out.println(id);
+        System.out.println(post.getTitle());
+        System.out.println(post.getContent());
+    }
+    @DeleteMapping({"/{id}"})
+    private void deletePost(@PathVariable Long id){
+        System.out.println("Deleting Post with ID: " + id);
+    }
+
+
 }
--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -1,12 +1,63 @@
+import fetchData from "../fetchData.js";
+
 export default function PostIndex(props) {
     return `
         <header>
             <h1>Posts Page</h1>
         </header>
         <main>
-            <div>
-                ${props.posts.map(post => `<h3>${post.title}</h3>`).join('')}   
+<!--MAKE CREATE FORM HERE--> 
+       <form>
+
+    <label for="username">Username</label>
+    <input id="username" name="username" type="text">
+    <br>
+
+    <label for="password">Password</label>
+    <input id="password" name="password" type="password">
+    <br>
+
+    <input type="button" id="create-post-btn">
+</form>
+            
+            <div class="post-container">
+                ${props.posts.map(post =>
+        `
+                        <h3>${post.title}</h3>
+                        <h2>${post.content}</h2>
+       <!--add edit, delete buttons, add edit form-->
+        
+        
+        `).join('')}   
             </div>
+            
+            
         </main>
     `;
+}
+//add event listeners, get data, send fetch
+//call function for edit btns listener
+//call function for data
+//call function for fetch
+export function PostsEvent() {
+    $('#create-post-btn').click(function () {
+
+        let post = {
+            title: $("#title").val(),
+            content: $("#content").val()
+        }
+        console.log(post)
+
+
+        let request = {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(post)
+        }
+        fetchData({
+                post: '/api/posts'
+            },
+            request)
+
+    });
 }
